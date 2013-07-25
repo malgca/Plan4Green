d4815f7d02b3fff0b66c301fbae89e2a376e93c4
Malusi Gcakasi <malusi.gcakasi@gmail.com>
2013-07-25 02:21:25 +0200

    Began Plan4Green System by starting initial UI designs.

---
+++ Plan4Green/Controllers/BSObjectController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plan4Green.Controllers
+{
+    public class BSObjectController : Controller
+    {
+        //
+        // GET: /BSObject/
+
+        public ActionResult PerspectiveViewer()
+        {
+            return View();
+        }
+
+        public ActionResult GoalViewer()
+        {
+            return View();
+        }
+
+        public ActionResult MeasureViewer()
+        {
+            return View();
+        }
+    }
+}
---
+++ Plan4Green/Controllers/GraphController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plan4Green.Controllers
+{
+    public class GraphController : Controller
+    {
+        //
+        // GET: /Graph/
+
+        public ActionResult GraphViewer()
+        {
+            return View();
+        }
+
+    }
+}
