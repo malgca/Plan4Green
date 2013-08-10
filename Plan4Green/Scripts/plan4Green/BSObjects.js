993c702d86acf4f1fdab765befa8dce9d24502e8
Malusi Gcakasi <malusi.gcakasi@gmail.com>
2013-08-10 20:43:17 +0200

    Resized canvas to fit on smaller screens. Began javascript for drawing BS Objects.

--- Plan4Green/Scripts/plan4Green/BSObjects.js
+++ Plan4Green/Scripts/plan4Green/BSObjects.js
@@ -1,5 +1,27 @@
-﻿function Perspective(name, description, indicator) {
+﻿/*-------------------------------------------------------
+TYPE DEFINITIONS
+-------------------------------------------------------*/
+// bs perspective object.
+function Perspective(name, description)
+{
     this.name = name;
     this.description = description;
-    this.indicator = indicator;
+}
+
+// bs goal object.
+function Goal(name, dueDate, description, target)
+{
+    this.name = name;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.target = target;
+}
+
+// bs measure object.
+function Measure(name, dueDate, description, target)
+{
+    this.name = name;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.target = target;
 }
--- Plan4Green/Scripts/plan4Green/main.js
+++ Plan4Green/Scripts/plan4Green/main.js
@@ -1,1 +1,71 @@
-﻿
+﻿/*-----------------------------------------------------------------
+GLOBAL VARIABLES
+-------------------------------------------------------------------*/
+
+var canvas, context
+
+/*------------------------------------------------------------------
+TYPE DEFINITIONS
+-------------------------------------------------------------------*/
+
+// representation of a point on the client.
+function Point(xPosition, yPosition)
+{
+    this.x = xPosition;
+    this.y= yPosition;
+}
+
+/*---------------------------------------------------------------------
+DOM EVENTS
+----------------------------------------------------------------------*/
+function initialize()
+{
+    // get drawing canvas and assign corresponding context
+    canvas = document.getElementById('drawing-canvas');
+    context = canvas.getContext('2d');
+
+    // add event listeners to canvas
+    canvas.addEventListener("mousedown", getPosition, false);
+}
+
+// gets the position of a mouse event on the screen.
+function getPosition(event)
+{
+    // position on the screen.  
+    var pos = new Point(event.x, event.y);
+
+    pos.x -= canvas.offsetLeft;
+    pos.y -= canvas.offsetTop;
+    return pos;
+}
+
+/*------------------------------------------------------------
+DRAWING METHODS
+--------------------------------------------------------------*/
+// draw a perspective BS Object
+function drawPerspective()
+{
+    var pos = getPosition(event);
+    //draw oval wrapper
+    context.fillStyle = "rgba(0, 0, 200, 1)";
+    context.fillRect(pos.x, pos.y, 75, 70);
+    alert("x: " + pos.x + " y: " + pos.y);
+}
+
+// draw a goal BS Object
+function drawGoal(goal)
+{
+    // draw sextagon wrapper
+}
+
+// draw a measure BS Object
+function drawMeasure(measure)
+{
+    // draw triangle wrapper
+}
+
+function drawIndicator(bsObject, name)
+{
+    var type = typeof (bsObject);
+    alert(type);
+}
