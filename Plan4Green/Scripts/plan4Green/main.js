0732fb19a4773f8100cfb5e461d5c0e32625ea86
Malusi Gcakasi <malusi.gcakasi@gmail.com>
2013-08-12 17:14:12 +0200

    Broke up main JS into different files. Continued to work on singular BSObject class.

--- Plan4Green/Scripts/plan4Green/main.js
+++ Plan4Green/Scripts/plan4Green/main.js
@@ -1,18 +1,23 @@
-﻿// Variables used while building app.
-var isDebugging = true;
-var position = document.getElementById("position");
-var count = 0;
+﻿/*-----------------------------------------------------------------
+BUILDING VARIABLES
+-------------------------------------------------------------------*/
+// flag set while creatings site.
+var isUnderConstruction = true;
 
+// elements to show building information
+var position = document.getElementById("position");
+var status = document.getElementById("status");
+
+// dummy count and object size
+var count = 0;
 var classSize = 100;
+
 /*-----------------------------------------------------------------
-GLOBAL VARIABLES
+SCRIPT VARIABLES
 -------------------------------------------------------------------*/
 
 // page upon which stuff will be drawn.
 var page;
-
-// represents whether or not the app is dragging.
-var isDragging;
 
 // array of existing BS objects.
 var bsObjects;
@@ -31,20 +36,25 @@ function initialize()
 
     // clear the starting array.
     bsObjects = new Array();
-
+    
     // add mouse event listeners to page
     page.addEventListener("mousedown", mousepressed, false);
     page.addEventListener("mouseup", mousereleased, false);
     page.addEventListener("mousemove", mousedragged, false);
     page.addEventListener("mouseout", mousecancelled, false);
+
+    createBSObject('Perspective');
+    createBSObject('Perspective');
 }
 
+function createBSObject(type)
+{
+    count++;
+    new bsobject(type, count, classSize);
+}
 // mousedown event handler
 function mousepressed(event)
 {
-    new Perspective('financial', null, 200, 200);
-    drawPerspective();
-    getPosition(event);
 }
 
 // mouseup event handler
@@ -55,10 +65,11 @@ function mousereleased(event)
 // mousemove event handler
 function mousedragged(event)
 {
-    if (isDebugging)
+    if (isUnderConstruction)
     {
         var pos = getPosition(event);
         position.innerHTML = "(x: " + pos.x + " | y: " + pos.y + ")";
+        
     }
 }
 
@@ -67,122 +78,18 @@ function mousecancelled(event)
 {
 }
 
-/*------------------------------------------------------------
-DRAWING METHODS
---------------------------------------------------------------*/
-// create the canvas upon which bs objects will be drawn.
-function createCanvas(name, width, height)
+/*------------------------------------------------------------------------
+PAGE METHODS
+-------------------------------------------------------------------------*/
+// Returns the current level of the app.
+function getCurrentLevel(name, type)
 {
-    // get drawing canvas
-    canvas = document.createElement('canvas');
-
-    // set the canvas attribute tags
-    canvas.setAttribute('id', name + "-canvas");
-    canvas.setAttribute('width', width + 20);
-    canvas.setAttribute('height', height + 20);
-
-    return canvas;
+    if (typeof type == null)
+    {
+        return "All Perspectives";
+    }
+    else
+    {
+        return typeof type + ": " + name;
+    }
 }
-
-// create the parent div with inner canvas'.
-function createDiv(xPosition, yPosition)
-{
-    // create the outer wrapper div.
-    div = document.createElement('div');
-    div.setAttribute('id', '-outer-wrapper');
-    div.className = 'bsobject';
-
-    // create the inner canvas with dummy id
-    var canvas = createCanvas('bsObject' + count, classSize, classSize)
-    count++;
-
-    // set the div to the mouse click 
-    div.style.left = xPosition + 'px';
-    div.style.top = yPosition + 'px';
-
-    // append inner canvas to outer div
-    div.appendChild(canvas);
-    return div;
-}
-
-// draw a perspective BS Object
-function drawPerspective(perspective)
-{
-    var pos = getPosition(event);
-
-    div = createDiv(pos.x, pos.y);
-    // get the canvas context and draw the perspective outline
-    var context = canvas.getContext('2d');
-    refreshCanvas;
-    
-    context.strokeStyle = "#000000";
-    context.strokeRect(pos.x, pos.y, classSize, classSize);
-    context.fillStyle = "#ffffff";
-    context.fillRect(pos.x, pos.y, classSize, classSize)
-
-    // place the div on the drawing page
-    page.appendChild(div);
-}
-
-    // draw a goal BS Object
-    function drawGoal(goal)
-    {
-        // draw sextagon wrapper
-
-    }
-
-    // draw a measure BS Object
-    function drawMeasure(measure)
-    {
-        // draw triangle wrapper
-    }
-
-    // draw the stoplight indicators for BS Objects.
-    function drawIndicator(type, name)
-    {
-        var type = typeof (bsObject);
-        if (typeof type == "Perspective")
-        {
-            alert(type);
-        }
-        else
-        {
-        }
-    }
-
-    // redraw the canvas whenever something get's done.
-    function refreshCanvas(canvas)
-    {
-        // clear the old stuff off the canvas.
-        context.clearRect(0, 0, canvas.scrollWidth, canvas.scrollHeight);
-
-        // draw shapes as they exist on the screen.   
-    }
-
-    /*------------------------------------------------------------------------
-    Other methods
-    -------------------------------------------------------------------------*/
-    // gets the position of a mouse event on the screen.
-    function getPosition(event)
-    {
-        // position on the screen.  
-        var pos = new Point(event.pageX, event.pageY);
-
-        pos.x -= page.offsetLeft;
-        pos.y -= page.offsetTop;
-
-        return pos;
-    }
-
-    // Returns the current level of the app.
-    function getCurrentLevel(name, type)
-    {
-        if (typeof type == null)
-        {
-            return "All Perspectives";
-        }
-        else
-        {
-            return typeof type + ": " + name;
-        }
-    }
---
+++ Plan4Green/Scripts/plan4Green/utilities.js
@@ -0,0 +1,14 @@
+﻿/*------------------------------------------------------------------------
+UTILITY METHODS
+-------------------------------------------------------------------------*/
+// gets the position of a mouse event on the screen.
+function getPosition(event)
+{
+    // position on the screen.  
+    var pos = new Point(event.pageX, event.pageY);
+
+    pos.x -= page.offsetLeft;
+    pos.y -= page.offsetTop;
+
+    return pos;
+}
